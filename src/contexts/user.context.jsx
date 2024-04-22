f7e7895a22f13230fd46fa944303101d4a8b3ad3
Shashank sullerey <msullerey@gmail.com>
2024-04-22 21:10:42 +0530

    added a functionality of checkout page in the application

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -11,6 +11,7 @@ export const UserContext = createContext({
   currentUser: null,
 });
 
+const a=10;
 export const UserProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
   const value = { currentUser, setCurrentUser };
